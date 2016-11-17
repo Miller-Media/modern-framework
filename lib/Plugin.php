a82d3aad17ac0e8ac778bfeaf3f5f2725ee170cf
Kevin Carwile <kcarwile@gmail.com>
2016-11-16 17:06:53 -0800

    added ability to pass custom variables to templates

--- lib/Plugin.php
+++ lib/Plugin.php
@@ -60,13 +60,13 @@ class Plugin extends Singleton
 	/**
 	 * Get a plugin file path
 	 *
-	 * @param	string		$pathfile		The file path and name (without extension)
-	 * @param	string		$type			The file type
+	 * @param	string				$pathfile		The file path and name
+	 * @param	string|FALSE		$type			The file type or FALSE if file type is included in $pathfile
 	 * @return	string
 	 */
 	public function pluginFile( $pathfile, $type='php' )
 	{
-		return $this->path . '/' . $pathfile . '.' . $type;
+		return $this->path . '/' . $pathfile . ( $type ? ( '.' . $type ) : '' );
 	}
 	
 	/**
@@ -81,28 +81,55 @@ class Plugin extends Singleton
 	}
 	
 	/**
-	 * Get Template Content
+	 * Get Template
 	 *
-	 * @param	string		$template 			The template to load (without file extension)
+	 * @param	string		$template 			Plugin template to look for (without file extension)
 	 * @return	string
 	 */
 	public function getTemplate( $template )
-	{			
-		ob_start();
-		
+	{
 		if ( $overridden_template = locate_template( $template . '.php' ) ) 
 		{
 			// locate_template() returns path to file
 			// if either the child theme or the parent theme have overridden the template
-			load_template( $overridden_template );
+			return $overridden_template;
 		} 
 		else 
 		{
 			// If neither the child nor parent theme have overridden the template,
 			// we load the template from the 'templates' sub-directory of the directory this file is in
-			load_template( dirname( __FILE__ ) . '/templates/' . $template . '.php' );
+			return $this->pluginFile( 'templates/' . $template );
+		}		
+	}
+
+	/**
+	 * Get Template Content
+	 *
+	 * @param	string		$template 			Plugin template to load (without file extension)
+	 * @param	array		$vars				Variables to extract and make available to template
+	 * @return	string
+	 */
+	public function getTemplateContent( $template, $vars=array() )
+	{
+		global $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
+		
+		if ( is_array( $wp_query->query_vars ) ) 
+		{
+			extract( $wp_query->query_vars, EXTR_SKIP );
 		}
 		
+		if ( is_array( $vars ) )
+		{
+			extract( $vars, EXTR_SKIP );
+		}
+
+		if ( isset( $s ) ) 
+		{
+			$s = esc_attr( $s );
+		}
+		
+		ob_start();
+		require $this->getTemplate( $template );
 		return ob_get_clean();
 	}
 	
