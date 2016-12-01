0b45c7980d6aca54378ec0e40eacb8b39bdf2733
Kevin Carwile <kcarwile@gmail.com>
2016-11-30 17:26:10 -0800

    added WP CLI command to download/update the boilerplate plugin

--- classes/CLI.php
+++ classes/CLI.php
@@ -125,4 +125,76 @@ class CLI extends \WP_CLI_Command {
 		
 		\WP_CLI::success( "Plugin successfully created in '{$assoc[ 'dir' ]}'." );
 	}
+	
+	/**
+	 * Update the wordpress plugin boilerplate
+	 *
+	 * ## OPTIONS
+	 *
+	 * <url>
+	 * : The download url of the current boilerplate to update to 
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # Update the boilerplate
+	 *     $ wp mwp update-boilerplate https://github.com/Miller-Media/wp-plugin-boilerplate/archive/master.zip
+	 *     Success: Boilerplate successfully updated.
+	 *
+	 * @subcommand update-boilerplate
+	 * @when after_wp_load
+	 */
+	function updateBoilerplate( $args, $assoc ) 
+	{
+		include_once( ABSPATH . 'wp-admin/includes/file.php' ); // Internal Upgrader WP Class
+		include_once( ABSPATH . 'wp-admin/includes/class-wp-upgrader.php' ); // Internal Upgrader WP Class
+		
+		if ( WP_Filesystem() == FALSE )
+		{
+			\WP_CLI::error( 'Error initializing wp_filesystem.' );
+			return;
+		}
+		
+		$download_url = $args[0];
+		$upgrader = new \WP_Upgrader;
+		$framework = \Modern\Wordpress\Framework::instance();
+
+		ob_start();
+		
+		\WP_CLI::line( 'Downloading package...' );
+		/*
+		 * Download the package (Note, This just returns the filename
+		 * of the file if the package is a local file)
+		 */
+		if ( is_wp_error( $download = $upgrader->download_package( $download_url ) ) ) 
+		{
+			\WP_CLI::error( $download );
+		}
+
+		$delete_package = ( $download != $download_url ); // Do not delete a "local" file
+
+		\WP_CLI::line( 'Extracting package...' );
+
+		// Unzips the file into a temporary directory.
+		if ( is_wp_error( $working_dir = $upgrader->unpack_package( $download, $delete_package ) ) ) 
+		{
+			\WP_CLI::error( $working_dir );
+		}
+		
+		\WP_CLI::line( 'Updating boilerplate plugin...' );
+
+		// With the given options, this installs it to the destination directory.
+		$result = $upgrader->install_package( array
+		(
+			'source' => $working_dir,
+			'destination' => $framework->getPath() . '/boilerplate',
+			'clear_destination' => true,
+			'abort_if_destination_exists' => false,
+			'clear_working' => true,
+			'hook_extra' => array(),
+		) );
+		
+		$r = ob_get_clean();
+		
+		\WP_CLI::success( 'Boilerplate successfully updated.' );
+	}
 }
