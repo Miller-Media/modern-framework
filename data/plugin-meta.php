bcc01b75756bc4f1058f2f6985390e0f73ecc156
Kevin Carwile <kcarwile@gmail.com>
2016-12-11 02:09:09 -0800

    committing plugin meta

---
+++ data/plugin-meta.php
@@ -0,0 +1,13 @@
+<?php
+return <<<'JSON'
+{
+    "vendor": "Miller Media",
+    "namespace": "Modern\\Wordpress",
+    "slug": "modern-wordpress",
+    "name": "Modern Wordpress Plugin Framework",
+    "description": "Provides an object oriented development framework for modern wordpress plugins.",
+    "author": "Miller Media",
+    "author_url": "http:\/\/www.miller-media.com\/",
+    "version": "0.1.0"
+}
+JSON;
