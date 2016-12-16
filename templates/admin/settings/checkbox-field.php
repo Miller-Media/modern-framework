3c2dca4b98c3f4247ab487bdfe4d0cb241a0ee16
Kevin Carwile <kcarwile@gmail.com>
2016-12-16 13:47:02 -0800

    added admin checkbox field template

---
+++ templates/admin/settings/checkbox-field.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin HTML Template
+ *
+ * @var 	$settings		\Modern\Wordpress\Plugin\Settings			The settings store
+ * @var		$field			\Wordpress\Options\Field					The options field definition
+ */
+ 
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Access denied.' );
+}
+
+$currentValue = $settings->getSetting( $field->name );
+?>
+
+<label for="checkbox-<?php echo $field->name ?>">
+	<input type="hidden" name="<?php echo $settings->getStorageId() ?>[<?php echo $field->name ?>]" value="0" />
+	<input type="checkbox" id="checkbox-<?php echo $field->name ?>" name="<?php echo $settings->getStorageId() ?>[<?php echo $field->name ?>]" value="1" <?php if ( $currentValue ) { echo 'checked="checked"'; } ?> /> 
+	<?php echo $field->description; ?>
+</label>
+
