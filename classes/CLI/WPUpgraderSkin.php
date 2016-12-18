a64d3efe5efd3c3e744a2c490546b1542ee400f7
Kevin Carwile <kcarwile@gmail.com>
2016-12-18 05:54:36 -0800

    fixed wp cli output for update-boilerplate

---
+++ classes/CLI/WPUpgraderSkin.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Plugin Class File
+ *
+ * Created:   December 18, 2016
+ *
+ * @package:  Modern Framework for Wordpress
+ * @author:   Kevin Carwile
+ * @since:    {build_version}
+ */
+namespace Modern\Wordpress\CLI;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Access denied.' );
+}
+
+/** WP_Upgrader_Skin class */
+require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader-skin.php';
+
+/**
+ * WPUpgraderSkin Class
+ */
+class WPUpgraderSkin extends \WP_Upgrader_Skin
+{
+	/**
+	 * @access public
+	 */
+	public function header() {}
+
+	/**
+	 * @access public
+	 */
+	public function footer() {}
+	
+	/**
+	 *
+	 * @param string|WP_Error $errors
+	 */
+	public function error($errors) {}
+
+	/**
+	 *
+	 * @param string $string
+	 */
+	public function feedback($string) {}
+
+	/**
+	 * @access public
+	 */
+	public function before() {}
+
+	/**
+	 * @access public
+	 */
+	public function after() {}
+
+	/**
+	 * Output JavaScript that calls function to decrement the update counts.
+	 *
+	 * @since 3.9.0
+	 *
+	 * @param string $type Type of update count to decrement. Likely values include 'plugin',
+	 *                     'theme', 'translation', etc.
+	 */
+	protected function decrement_update_count( $type ) {}
+
+}
