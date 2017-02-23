bbde50693183de3cdaea8e81bba7db4f2f16f6d8
Jeremy Madison <jmadison1976@gmail.com>
2017-02-23 10:50:06 -0800

    Add multiselect field to admin

---
+++ templates/admin/settings/multiselect-field.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin HTML Template
+ *
+ * @var 	$settings		\Modern\Wordpress\Plugin\Settings			The settings store
+ * @var		$field			\Wordpress\Options\Field					The options field definition
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Access denied.' );
+}
+
+$selectOptions = $field->options ?: array();
+$currentValue = $settings->getSetting( $field->name ) ?: array();
+
+/**
+ * If select options is a string, see if it is a callable method on the settings store
+ * that can be used to generate the select options
+ */
+if ( is_string( $field->options ) )
+{
+	if ( is_callable( array( $settings, $selectOptions ) ) )
+	{
+		$selectOptions = call_user_func( array( $settings, $selectOptions ), $currentValue );
+	}
+}
+
+?>
+
+<select name='<?php echo $settings->getStorageId() ?>[<?php echo $field->name ?>][]' multiple <?php echo $field->getFieldAttributes() ?>>
+	<?php foreach( $selectOptions as $value => $title ) : ?>
+		<option value="<?php echo $value ?>" <?php if( in_array( $value, $currentValue ) ){ echo "selected"; } ?>>
+			<?php echo $title ?>
+		</option>
+	<?php endforeach; ?>
+</select>
