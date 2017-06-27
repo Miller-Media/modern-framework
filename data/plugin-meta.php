19631481452914938f516c07e91c7019f83f284a
Kevin Carwile <kcarwile@gmail.com>
2017-06-27 16:31:07 -0700

    1.3.2 build

--- data/plugin-meta.php
+++ data/plugin-meta.php
@@ -8,7 +8,7 @@ return <<<'JSON'
     "description": "Provides an object oriented utility framework for modern wordpress plugins.",
     "author": "Kevin Carwile",
     "author_url": "http:\/\/www.miller-media.com\/",
-    "version": "1.3.1",
+    "version": "1.3.2",
     "tables": "queued_tasks"
 }
 JSON;
