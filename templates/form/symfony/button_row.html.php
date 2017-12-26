2ba34fc687fad7f550ad1bdf085988b5b3dbbc26
Kevin Carwile <kcarwile@gmail.com>
2017-12-26 10:44:50 -0800

    adjusted button row to have configurable attributes

--- templates/form/symfony/button_row.html.php
+++ templates/form/symfony/button_row.html.php
@@ -14,6 +14,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<div>
+<?php echo $row_prefix ?>
+<div <?php 
+	foreach ($row_attr as $k => $v) { 
+		if ($v === true) {
+			printf('%s="%s" ', $view->escape($k), $view->escape($k));
+		} elseif ($v !== false){
+			printf('%s="%s" ', $view->escape($k), $view->escape($v));
+		} 
+	} ?>>
+	<?php echo $prefix; ?>
     <?php echo $view[ 'form' ]->widget( $form ) ?>
+	<?php echo $suffix ?>
 </div>
+<?php echo $row_suffix ?>
